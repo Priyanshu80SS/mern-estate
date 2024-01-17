de60f9c81b880b2faee3b438dbf35f81c51f4422
Priyanshu Bhardwaj <priyanshubhardwaj099@gmail.com>
2024-01-17 13:54:48 +0530

    Created pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
 
 const App = () => {
   return (
     <div>
-      <h1 className="text-red-500">hello</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="signin" element={<SignIn />} />
+          <Route path="signin" element={<SignOut />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
